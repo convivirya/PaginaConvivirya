242f8b8f07f148efd952ccfb0998ca9ab3f235ad
convivirya <conviveya205@gmail.com>
2023-04-22 18:41:58 -0500

    Commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'mysql-hugo170983.alwaysdata.net',
+        user: '278414_convivir',
+        password: 'Pululahua.170983',
+        database: 'hugo170983_convivirya'
+    }
+
+};
